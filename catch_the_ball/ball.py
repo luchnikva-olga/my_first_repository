d7bc57993afc6af537b90071e1a9cde9d9931a5c
luchnikva-olga <luchnikva-olga@rambler.ru>
2016-07-25 09:50:58 +0700

    пробую пользоваться сервером

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -1,6 +1,12 @@
 import tkinter
+
+def print_hello (event):
+    print ('Hello')
+    print (event)
+
 root=tkinter.Tk()
 button=tkinter.Button(root, text="Button")
+button.bind("<Button-1>",print_hello)
 button.pack()
 root.mainloop()
 
